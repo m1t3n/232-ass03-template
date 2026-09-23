22bf56ad0afcacf9e9a9a0ef6ec285c691458fbb
Mikayla Iten <miten@mtech.edu>
2026-09-23 16:58:04 -0600

    fixed authorship

--- src/code.c
+++ src/code.c
@@ -1,5 +1,9 @@
-//char * AUTHOR_NAME = (char *) "Jakub Pach";
-//char * AUTHOR_AUTHORSHIP = (char *) "I acknowledge that I have worked on this assignment independently, except where explicitly noted and referenced. Any collaboration or use of external resources has been properly cited. I am fully aware of the consequences of academic dishonesty and agree to abide by the university's academic integrity policy. I understand the seriousness and implications of plagiarism.";
+char *AUTHOR_NAME        = (char *) "Mikayla Iten";
+char *AUTHOR_AUTHORSHIP  = (char *) "I acknowledge that I have worked on this \
+assignment independently, except where explicitly noted and referenced. \
+Any collaboration or use of external resources has been properly cited. \
+I am fully aware of the consequences of academic dishonesty and agree to \
+abide by the university's academic integrity policy.";
 
 #include <stddef.h>
 #include <stdio.h>
